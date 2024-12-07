288e49c5a54ecf8388ef2078f1096b52b7202edd
Cristina-Figueroa <cristina.figueroa7@upr.edu>
2024-12-07 05:50:56 -0400

    added | to message

--- app/components/Home.js
+++ app/components/Home.js
@@ -209,7 +209,7 @@ function Home() {
       .then((data) => {
         console.log('Response from Flask:', data);
         setResponseMessage(
-          'Dia de Comienzo de Clases: ' + dateformatted + '     Periodo Academico Elegido: ' + academicPeriodFrontEnd
+          'Dia de Comienzo de Clases: ' + dateformatted + ' |    Periodo Academico Elegido: ' + academicPeriodFrontEnd
           // data.message + ' - Date:' + startDate + ' , Period: ' + period + ' , Weeks: ' + weeks
         );
         const processedDates = data.important_dates.map((dateItem) => {
